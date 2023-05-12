fd445d8021fe225884e36ab6d21d5d9436788c44
woominKO <woomin422@gmail.com>
2023-05-12 13:43:42 +0900

    correct onload

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-//1-20끼지 숫자 중에 추측게임
-//사용자의 입력값에 따라up down right 출력
-
-// console.log(playerNum);
-
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+  return Math.floor(Math.random() * (max - min + 1)) + min; // 최댓값과 최솟값을 모두 포함하도록 수정
 }
 
 let randomNum = getRandomInt(1, 20);
 
-while (true) {
+function guessNumber() {
   let playerNum = prompt(
     'guess number! 컴퓨터가 고른 1-20까지중의 숫자중 하나를 맞추세요!'
   );
   playerNum = parseInt(playerNum);
+
   if (playerNum == randomNum) {
-    alert('you are  right!');
-    break;
+    alert('You got the right answer!');
   } else if (playerNum > randomNum) {
     alert('down');
+    guessNumber(); // 다시 함수를 호출하여 사용자 입력 받음
   } else if (playerNum < randomNum) {
     alert('up');
+    guessNumber(); // 다시 함수를 호출하여 사용자 입력 받음
   } else {
     alert('enter number');
+    guessNumber(); // 다시 함수를 호출하여 사용자 입력 받음
   }
 }
+
+window.onload = guessNumber;
